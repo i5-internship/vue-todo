5b1204339705b7b098719b0050926fc5c1a7bfd4
NgoUyUong <ngouyoung@yahoo.com>
2019-03-14 14:59:33 +0700

    first commit

---
+++ src/views/layouts/Primary.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="primary">
+        <todo-navbar/>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import TodoNavbar from "../partials/TodoNavbar";
+    export default {
+        name: "Primary",
+        components: {TodoNavbar}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/partials/TodoNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="Navbar">
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-container>
+                <b-navbar-brand >Todo list</b-navbar-brand>
+                <b-navbar-brand >New Todo</b-navbar-brand>
+
+                <b-navbar-toggle target="nav_collapse"/>
+
+                <b-collapse is-nav id="nav_collapse">
+                </b-collapse>
+            </b-container>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoNavbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
